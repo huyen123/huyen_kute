3d013ff2b14d5445f69c228189d0d350ed7334b9
your name <huyen@huyen-nguyen.(none)>
2013-04-04 10:48:19 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HuyenKute::Application.config.secret_token = 'bc1568c6326caf3dd764b4282c33e6e197c8556969f5c577b3b65c88459ea43feb72f5a4a2dbfa4eb74407cb564be61a6178c6bc23763feba142b8049c089188'
